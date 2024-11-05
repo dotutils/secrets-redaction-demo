c56cf526bd0f4a052fef3555bc7d51675cfdeafb
YuliiaKovalova <95473390+YuliiaKovalova@users.noreply.github.com>
2024-11-05 13:30:38 +0000

    Cleanup of imported packages (#11)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BuilderApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Update!");
+        }
+    }
+}
